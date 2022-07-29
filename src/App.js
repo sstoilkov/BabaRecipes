b7be75d792de27101fd94b2d777d246431cf20ab
Svetlozar <svetly91@gmail.com>
2022-07-29 16:48:08 +0300

    add styles and home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Header } from "./components/Header";
+import { Home } from "./components/Home/Home";
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <main id="main-content">
+        <Home />
+      </main>
     </div>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,17 @@
+
+
+export const Home = () => {
+    return (
+        <section id="welcome-world">
+            <div className="welcome-message">
+                <h2>ALL new recipes are</h2>
+                <h3>in Baba Recipes</h3>
+            </div>
+            {/* <img src="./images/four_slider_img01.png" alt="" /> */}
+            <div id="home-page">
+                <h1>Latest Recipes</h1>
+                <p className="no-articles">No recipes yet</p>
+            </div>
+        </section>
+    );
+}
