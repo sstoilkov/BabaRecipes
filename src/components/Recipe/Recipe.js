ba33f4468bc8dc72164859ed096fd3cc97bd7283
Svetlozar <svetly91@gmail.com>
2022-07-29 21:27:48 +0300

    Recipe and RecipeItem added

---
+++ src/components/Recipe/Recipe.js
@@ -0,0 +1,12 @@
+import RecipeItem from "./RecipeItem/RecipeItem"
+
+export const Recipe = () => {
+    return (
+        <section id="recipe-page">
+            <h1>All Recipes</h1>
+
+            <h3 className="no-recipe">No recipe yet</h3>
+
+        </section>
+    );
+};
---
+++ src/components/Recipe/RecipeItem/RecipeItem.js
@@ -0,0 +1,15 @@
+
+export const RecipeItem = () => {
+    return (
+        <div className="allRecipes">
+            <div className="allRecipes-info">
+                <img src="https://2.bp.blogspot.com/-K7Q8Fyl4r5U/VsT-pn9R6XI/AAAAAAAA39I/tVaT7aHOzLQ/s1600/pizza.gif" />
+                <h6>Category: Pizza</h6>
+                <h2>Margarita</h2>
+                {/* <Link to={`/catalog/${game._id}`} className="details-button">
+                    Details
+                </Link> */}
+            </div>
+        </div>
+    );
+};
