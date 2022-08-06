2111e4e16b6a0ad53549a73b0cac6c8b10107759
Svetlozar <svetly91@gmail.com>
2022-08-06 19:15:14 +0300

    AuthContext
    All functionality moved from App to AuthContext

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,5 +1,32 @@
-
+import React from "react";
+import { useLocalStorate } from "../hooks/useLocalStorage";
 import { createContext } from "react";
 
 export const AuthContext = createContext();
 
+export const AuthProvider = ({
+    children,
+}) => {
+
+    const [auth, setAuth] = useLocalStorate('auth', {});
+
+    const userLogin = (authData) => {
+        setAuth(authData)
+    }
+
+
+    const userLogout = () => {
+        setAuth({});
+    }
+
+    return (
+        <AuthContext.Provider value={{
+            user: auth,
+            userLogin,
+            userLogout,
+            isAuthenticated: !!auth.accessToken
+        }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
