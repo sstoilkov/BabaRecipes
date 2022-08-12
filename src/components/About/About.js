0a03653eed29052baf256d94c23967ca546e6eed
Svetlozar <svetly91@gmail.com>
2022-08-12 20:29:30 +0300

    Css updates

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,10 +5,10 @@ import styles from "./About.module.css"
 export const About = () => {
     return (
         <section id="aboutPage" className={styles.aboutPage}>
-            <div className={styles.aboutPage}>
-                <h1>This is a cooking application for people who love cooking. </h1>
-                <h2>Please login to get the best cooking experience!</h2>
-            </div>
+
+            <h1>This is a cooking application for people who love cooking. </h1>
+            <h2>Please login to get the best cooking experience!</h2>
         </section>
+
     );
 }
--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -5,22 +5,14 @@ import styles from "./Contacts.module.css"
 export const Contacts = () => {
     return (
         <section id="contactsPage" className={styles.contactsPage}>
-            <div className={styles.contactsPage}>
-                <ul>
-                    <li>
-                        <p>You can contact us by email:</p>
-                    </li>
-                    <li>
-                        <p>clients@baba-recipes.com</p>
-                    </li>
-                    <li>
-                        <p> or by phone on:</p>
-                    </li>
-                    <li>
-                        <p>  088 88 888 88 </p>
-                    </li>
-                </ul>
-            </div>
+            <ul>
+                <li>
+                    <p>You can contact us by email: clients@baba-recipes.com</p>
+                </li>
+                <li>
+                    <p> or by phone on: 088 88 888 88</p>
+                </li>
+            </ul>
         </section >
     );
 }
